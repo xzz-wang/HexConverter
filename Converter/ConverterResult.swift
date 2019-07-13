30b46d2875ca1d75dd3400cd99490d93bafacb3f
Xuezheng Wang <xzzwang@icloud.com>
2019-07-12 20:23:45 -0700

    Copied some code from apple's tutorial about persistence

--- Converter/ConverterResult.swift
+++ Converter/ConverterResult.swift
@@ -8,13 +8,48 @@
 
 import Foundation
 import SwiftUI
+import os.log
 
-public class ConverterResult: Identifiable {
+struct PropertyKey {
+    static let value = "value"
+    static let base = "base"
+}
+
+public class ConverterResult: NSObject, NSCoding, Identifiable {
+    
+    
+    // MARK: Properties
+    
     var value: Int
     var base: HexConverter.BaseTypes
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    // Normal Initializer
     
     init(of value: Int, in base: HexConverter.BaseTypes) {
         self.value = value
         self.base = base
     }
+    
+    // MARK: NSCoding
+    public func encode(with coder: NSCoder) {
+        coder.encode(value, forKey: PropertyKey.value)
+        coder.encode(base, forKey: PropertyKey.base)
+    }
+    
+    public required init?(coder: NSCoder) {
+        let value = coder.decodeObject(forKey: PropertyKey.value)
+        let base = coder.decodeObject(forKey: PropertyKey.base)
+        
+        if value == nil || base == nil {
+            os_log("Can not load the properties", type: .debug)
+            return nil
+        }
+        
+        self.value = value as! Int
+        self.base = base as! HexConverter.BaseTypes
+    }
+
 }
